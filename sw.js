fa797c5ee0d19053c97e714b70e6d8fb549f09a4
ankurkumar-ship-it <vineshkumarsingh32@gmail.com>
2026-06-29 12:50:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
+const CACHE_NAME = 'peersolve-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+// Install karte time files save karna (Offline ke liye)
 self.addEventListener('install', (event) => {
-    console.log('App successfully install ho gaya!');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// App ko bina internet chalane ka logic
 self.addEventListener('fetch', (event) => {
-    // Yeh background mein website ko app jaisa feel dega
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
