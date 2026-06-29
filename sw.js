ebde5cd63305b63cc227d880e73a8ee6598e2897
ankurkumar-ship-it <vineshkumarsingh32@gmail.com>
2026-06-29 11:36:33 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (event) => {
+    console.log('App successfully install ho gaya!');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Yeh background mein website ko app jaisa feel dega
+});
